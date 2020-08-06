5de70ffa7abcdc35852c79ffd1a0a5566a0de7d7
kai-haru <69281418+kai-haru@users.noreply.github.com>
2020-08-06 11:10:22 +0900

    行の修正

--- Ex4.c
+++ Ex4.c
@@ -3,15 +3,13 @@
 
 int main(){
   int i,head = 0,tail = 0,HoT;
+  
   char s[10];
   printf("Who are you?\n");
   scanf("%s",s);
   printf("Hello,%s\n",s);
   
   printf("Tossing a coin...\n");
-
- 
-
   for(i = 1;i<4;i++){
     HoT = rand();
     if(HoT%2==0){
